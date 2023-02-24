27ff4a19f1cadbee3cd8d7ce6627f24d72b9dcc4
mahammadam <mahammadam@code.edu.az>
2023-02-24 17:14:53 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿int X = 100;
+ if (X >=91 && X <=100)
+    Console.WriteLine("A");
+if (X >= 81 && X <= 90)
+    Console.WriteLine("B");
+if (X >= 71 && X <= 80)
+    Console.WriteLine("C");
+if (X >= 61 && X <= 70)
+    Console.WriteLine("D");
+if (X >= 51 && X <= 60)
+    Console.WriteLine("E");
+else if (X < 51)
+    Console.WriteLine("Kəsildiniz");
+
+
+
+     
